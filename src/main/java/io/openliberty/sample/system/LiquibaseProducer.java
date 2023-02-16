80c30791617cd901e7023d4c260db6dcd6282bf6
gituser <catchingmouse@gmail.com>
2023-02-16 16:54:57 +0100

    Initial commit

---
+++ src/main/java/io/openliberty/sample/system/LiquibaseProducer.java
@@ -0,0 +1,36 @@
+package io.openliberty.sample.system;
+
+import javax.annotation.Resource;
+import javax.enterprise.inject.Produces;
+import liquibase.integration.cdi.CDILiquibaseConfig;
+import liquibase.integration.cdi.annotations.LiquibaseType;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class LiquibaseProducer {
+    @Resource
+    private DataSource myDataSource;
+
+    @Produces
+    @LiquibaseType
+    public CDILiquibaseConfig createConfig() {
+        CDILiquibaseConfig config = new CDILiquibaseConfig();
+        config.setChangeLog("liquibase/parser/core/xml/simpleChangeLog.xml");
+        return config;
+    }
+
+    @Produces
+    @LiquibaseType
+    public DataSource createDataSource() throws SQLException {
+        return myDataSource;
+    }
+
+    @Produces
+    @LiquibaseType
+    public ResourceAccessor create() {
+        return new ClassLoaderResourceAccessor(getClass().getClassLoader());
+    }
+}
